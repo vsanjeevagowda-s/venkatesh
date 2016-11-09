0df2bf5470af99bc7566e86905f247f0b1cf6d0e
venkatesh s <vsanjeevagowda@qwinix.io>
2016-11-09 18:17:39 +0530

    rails checkbox added

--- BackEnd/rails/27/library_managment/app/assets/javascripts/application.js
+++ BackEnd/rails/27/library_managment/app/assets/javascripts/application.js
@@ -14,3 +14,43 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$('document').ready(function(){
+$('.pub').click(function(){
+	$('.pub').css('color','black');
+});
+
+var index = 0;
+	slideShow();
+	
+	function slideShow(){
+	
+	var slide1=document.getElementsByClassName("myImage");
+	var text1=document.getElementsByClassName("homeTextValue");
+	for(var i=0;i<slide1.length;i++){
+		slide1[i].style.display="none";
+		text1[i].style.display="none";
+
+	}
+
+	
+	var slide2=document.getElementsByClassName("myImage");
+	var text2=document.getElementsByClassName("homeTextValue");
+	if(index+1>slide1.length){
+		index=0;
+	}
+
+	slide2[index].style.display="block";
+	text2[index].style.display="block";
+	index++;
+	setTimeout(slideShow, 4000);
+
+	
+}
+
+
+
+});
+
+
+
